467f0299c60ebcae4a05f51abffa44171a7e9b05
Shubham <shubham.jha@tekditechnologies.com>
2024-11-04 16:18:55 +0530

    Remove Writing File

--- databaseQueries/solutions.js
+++ databaseQueries/solutions.js
@@ -39,11 +39,7 @@ module.exports = class Solutions {
             projection[field] = 0;
           });
         }
-        require('fs').writeFileSync('queryObj.json',JSON.stringify({
-          queryObject, projection
-        }))
         let solutionsDoc = await database.models.solutions.find(queryObject, projection).lean();
-
         return resolve(solutionsDoc);
       } catch (error) {
         return reject(error);
